70959846af42beff0d74f9ff0b4fd3fc27a1489d
LordYeahNah <144700841+LordYeahNah@users.noreply.github.com>
2023-12-11 11:27:29 +1100

    Created Timer Class

---
+++ Assets/Scripts/Core/Timer.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class Timer
+{
+    public float TimerLength;
+    private float _CurrentTime;
+    public bool IsActive = true;
+    public bool Loop;
+    public event Action TimerCompleteActions;
+
+    public Timer(float length, Action completeAction, bool loop = false, bool isActive = true)
+    {
+        TimerLength = length;
+        TimerCompleteActions += completeAction;
+        Loop = loop;
+        IsActive = isActive;
+    }
+
+    public void OnUpdate()
+    {
+        _CurrentTime += 1 * Time.deltaTime;
+        if(_CurrentTime > TimerLength)
+            _CompleteTimer();
+    }
+
+    private void _CompleteTimer()
+    {
+        TimerCompleteActions?.Invoke();
+        _CurrentTime = 0f;
+        IsActive = Loop;
+    }
+
+    public void ResetTimer(bool isActive = true)
+    {
+        _CurrentTime = 0f;
+        IsActive = isActive;
+    }
+}
