cef2df8089b3aae506a36bdea60c102937fe0c50
LordYeahNah <144700841+LordYeahNah@users.noreply.github.com>
2023-12-14 11:36:20 +1100

    Created the initial task

---
+++ Assets/Scripts/AI/Task.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ETaskStatus
+{
+    SUCCESS = 0,
+    FAILURE = 1,
+    RUNNING = 2
+}
+
+public abstract class Task
+{
+    public BehaviorTree _Tree;
+    public Task Parent;
+    public List<Task> _Children = new List<Task>();
+
+    public Task(BehaviorTree tree)
+    {
+        _Tree = tree;
+    }
+
+    public Task(BehaviorTree tree, List<Task> children)
+    {
+        _Tree = tree;
+
+        foreach(var child in children)
+            _AttachChild(child);
+    }
+
+    private void _AttachChild(Task child)
+    {
+        _Children.Add(child);
+        child.Parent = this;
+    }
+
+    public abstract ETaskStatus RunTask();
+}
