48caa899cd2a6a6d53903532946de662534574bb
LordYeahNah <144700841+LordYeahNah@users.noreply.github.com>
2023-12-15 09:18:39 +1100

    Created general keys that are used globally

---
+++ Assets/Scripts/AI/GeneralBlackboardKeys.cs
@@ -0,0 +1,9 @@
+using System.Numerics;
+
+public static class GeneralKeys
+{
+    public static readonly string SELF = "Self";
+    public static readonly string MOVE_TO_LOCATION = "MoveToLocation";
+    public static readonly string TARGET = "Target";
+    public static readonly string WORKING_ROOM = "WorkingRoom";
+}
